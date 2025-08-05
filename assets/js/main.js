d32c8e245e8e856e3ba641cdc8fd5bf7e45b8801
yarasa27 <elcan.abbasoff27@gmail.com>
2025-08-06 02:52:31 +0400

    login html css and JS for open and close finished and starting section home

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,21 @@ if(searchClose) {
 }
 
 /*=============== LOGIN ===============*/
+const loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close'),
+      loginContent = document.getElementById('login-content')
 
+if(loginButton) {
+    loginButton.addEventListener('click', () => {
+        loginContent.classList.add('show-login')
+    })
+}
+
+if(loginClose) {
+    loginClose.addEventListener('click', () => {
+        loginContent.classList.remove('show-login')
+    })
+}
 
 /*=============== ADD SHADOW HEADER ===============*/
 
