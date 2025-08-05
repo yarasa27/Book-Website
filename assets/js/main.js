2ba0acd6182ba3bd8c90142b5d7e03361f4baa33
yarasa27 <elcan.abbasoff27@gmail.com>
2025-08-06 01:03:06 +0400

    search add and remove in JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
-/*=============== SEARCH ===============*/
+/* SEARCH */
+const searchButton = document.getElementById('search-button'),
+      searchClose = document.getElementById('search-close'),
+      searchContent = document.getElementById('search-content')
 
+if(searchButton) {
+    searchButton.addEventListener('click', () => {
+        searchContent.classList.add('show-search')
+    })
+}
+
+if(searchClose) {
+    searchClose.addEventListener('click', () => {
+        searchContent.classList.remove('show-search')
+    })
+}
 
 /*=============== LOGIN ===============*/
 
