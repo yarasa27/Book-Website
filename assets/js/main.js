97006a69ce9880dde438c744332f1c0844c03d0b
yarasa27 <elcan.abbasoff27@gmail.com>
2025-08-09 17:07:46 +0400

    scrollbar and scrollup css html JS finished and starting scroll sections active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,7 +112,12 @@ let swiperTestimonial = new Swiper('.testimonial__swiper', {
 });
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
